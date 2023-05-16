105cda3bb27ef440056ad702d93316c677a58218
Jenny <jennyandrus1@gmail.com>
2023-05-15 21:41:06 -0400

    Initial Commit

---
+++ ProjectWellnessWizards/ProjectWellnessWizardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectWellnessWizardsApp.swift
+//  ProjectWellnessWizards
+//
+//  Created by Jenny Andrus on 5/15/23.
+//
+
+import SwiftUI
+
+@main
+struct ProjectWellnessWizardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
